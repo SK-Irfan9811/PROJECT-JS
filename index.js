b3333684c68df539a4fc9a98e16ef0f79fe2906f
Irfan <isk22802@gmail.com>
2022-10-15 21:54:58 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+var w=document.getElementById("watch");
+w.style.cursor="pointer";
+w.addEventListener('click',function(){
+    window.location.href="SmartWatchProject.html";
+   
+});
+var F=document.getElementById("form");
+F.style.cursor="pointer";
+F.addEventListener('click',function(){
+    window.location.href="MultiStepForm.html";
+})
